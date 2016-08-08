98cc8695ae6dcc0dbb3ff0a55eb00bef6271935b
Kevin Ivy <kevinivy1@gmail.com>
2016-08-08 11:50:06 -0500

    Created oncreate for buttons in mainactivity. Created detailsactivity

--- app/src/main/java/com/lsukev/flaborfit/MainActivity.java
+++ app/src/main/java/com/lsukev/flaborfit/MainActivity.java
@@ -2,12 +2,45 @@ package com.lsukev.flaborfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXERCISE_WEIGHTS="Weight lifting";
+    public static final String EXERCISE_YOGA = "Yoga";
+    public static final String EXERCISE_CARDIO = "Cardio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);sadasd
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout btnWeight = (RelativeLayout) findViewById(R.id.btnWeight);
+        RelativeLayout btnYoga = (RelativeLayout) findViewById(R.id.btnYoga);
+        RelativeLayout btnCardio = (RelativeLayout) findViewById(R.id.btnCardio);
+
+        btnWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXERCISE_WEIGHTS);
+            }
+        });
+        btnCardio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXERCISE_CARDIO);
+            }
+        });
+        btnYoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(MainActivity.EXERCISE_YOGA);
+            }
+        });
+    }
+
+    private void loadDetailActivity(String exerciseTitle){
+
     }
 }
